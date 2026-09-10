280745a36a681a51bfbb175de4b7d7c66d37134e
Inara Oenning <inaraoenning@gmail.com>
2026-09-09 22:34:52 -0300

    Remove dados db

--- src/database/DbConnection.kt
+++ src/database/DbConnection.kt
@@ -5,9 +5,9 @@ import java.sql.DriverManager
 
 object DbConnection {
     private val url: String =
-        "jdbc:postgresql://dpg-dafuit7qj5pc738e0kbg-a.oregon-postgres.render.com/dbgerenciador?sslmode=require"
-    private val user: String = "admin"
-    private val senha: String = "RgI9R6NyTQJQmEyGOkquMnWMeS7eN4u1"
+        ""
+    private val user: String = ""
+    private val senha: String = ""
 
     // Retorna a conexão ao invés de guardar numa variável global mutável
     fun conectar(): Connection {
